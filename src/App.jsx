f5e9fa2eebee6ff0879c7da4b078ea1ddcfb9a83
Oleksandr Prokopchuk <sasha195@i.ua>
2025-01-19 17:53:36 +0200

    Add 1-2 task

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import ForbesList from './components/ForbesList/ForbesList';
 import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
+import article from './data/article.json';
+import stats from './data/stats.json';
 
 export const App = () => {
   return (
     <Section>
       <Container>
         <Heading title="Task 1 Blog Card" bottom />
-        <BlogCard />
+        <BlogCard {...article} />
 
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
+        <Statistics title="Main Statistics" stats={stats} />
+        <Statistics stats={stats} />
 
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList />
